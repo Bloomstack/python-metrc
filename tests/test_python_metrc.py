dfc12f287c8dcc7976e0c8fbddea5deea414a776
Rohan Bansal <rohan@digithinkit.com>
2018-10-08 12:50:58 +0530

    Fix tests + flake8 errors

--- tests/test_python_metrc.py
+++ tests/test_python_metrc.py
@@ -16,7 +16,8 @@ URL = BASE_URL + PATH
 
 @pytest.fixture(scope="module")
 def metrc():
-    return METRC(BASE_URL, vendor_key="", user_key="", license_number="", api_version="v2")
+    return METRC(BASE_URL, vendor_key="", user_key="",
+                 license_number="", api_version="v2")
 
 
 def test_urls(metrc):
